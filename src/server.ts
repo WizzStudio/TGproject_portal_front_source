471f61a7b9aba68a499f7cebebcd2d9420652415
ACERY1 <y841034081@gmail.com>
2017-11-13 20:49:31 +0800

    build the home page

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,7 @@ import * as errorHandler from "errorhandler";
 import * as dotenv from "dotenv";
 import * as path from "path";
 import expressValidator = require("express-validator");
+import CUtil from './utils/Utils'
 
 /**
  * express server
@@ -24,11 +25,122 @@ app.set("views", path.join(__dirname, "../views"));  // 绑定MVC中的View层
 app.set("view engine", "pug");  // 使用渲染引擎
 app.use(logger("dev"));  // 使用express 自带 logger -Morgan /*dev common combined short tiny*/
 app.use(bodyParser.json());  // 处理http请求body里的application/json数据
-app.use(bodyParser.urlencoded({ extended: false }));  // for application/x-www-form-urlencoded
-app.use(express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })); // 使用express静态转发，/js将转发到/public/js
+app.use(bodyParser.urlencoded({extended: false}));  // for application/x-www-form-urlencoded
+app.use(express.static(path.join(__dirname, "public"), {maxAge: 31557600000})); // 使用express静态转发，/js将转发到/public/js
 app.use(expressValidator());
 
 
+let tempData = {
+    active: 'home',
+    newProjects: [
+        {
+            pic: '/images/bg13.png',
+            intro: 'Win8是Window通用平台开发平台的起点，Win8是Window通用平台开发平台的起点',
+            title: '基于Win8的新一代应用平台',
+            startTime: CUtil.ms2Date(1510558487737),
+            progress: {
+                front: 65,
+                back: 40,
+                pm: 50,
+                total: 48
+            },
+            leader: 'shitman',
+        },
+        {
+            pic: '/images/bg13.png',
+            intro: '人类误判心理学——21种常见心理倾向',
+            title: '基于人工智能的图片识别技术',
+            startTime: CUtil.ms2Date(1510558487737),
+            progress: {
+                front: 65,
+                back: 40,
+                pm: 50,
+                total: 48
+            },
+            leader: 'fuckman',
+        }
+    ],
+    focusProjects: [
+        {
+            pic: '/images/bg13.png',
+            intro: 'Win8是Window通用平台开发平台的起点，Win8是Window通用平台开发平台的起点',
+            title: '基于Win8的新一代应用平台',
+            startTime: CUtil.ms2Date(1510558487737),
+            progress: {
+                front: 65,
+                back: 40,
+                pm: 50,
+                total: 48
+            },
+            leader: 'shitman',
+        },
+        {
+            pic: '/images/bg13.png',
+            intro: 'Win8是Window通用平台开发平台的起点，Win8是Window通用平台开发平台的起点',
+            title: '基于人工智能的图片识别技术',
+            startTime: CUtil.ms2Date(1510558487737),
+            progress: {
+                front: 65,
+                back: 40,
+                pm: 50,
+                total: 48
+            },
+            leader: 'fuckman',
+        },
+        {
+            pic: '/images/bg13.png',
+            intro: 'Win8是Window通用平台开发平台的起点，Win8是Window通用平台开发平台的起点',
+            title: '基于人工智能的图片识别技术',
+            startTime: CUtil.ms2Date(1510558487737),
+            progress: {
+                front: 65,
+                back: 40,
+                pm: 50,
+                total: 48
+            },
+            leader: 'fuckman',
+        },
+        {
+            pic: '/images/bg13.png',
+            intro: 'Win8是Window通用平台开发平台的起点，Win8是Window通用平台开发平台的起点',
+            title: '基于人工智能的图片识别技术',
+            startTime: CUtil.ms2Date(1510558487737),
+            progress: {
+                front: 65,
+                back: 40,
+                pm: 50,
+                total: 48
+            },
+            leader: 'fuckman',
+        },
+        {
+            pic: '/images/bg13.png',
+            intro: 'Win8是Window通用平台开发平台的起点，Win8是Window通用平台开发平台的起点',
+            title: '基于人工智能的图片识别技术',
+            startTime: CUtil.ms2Date(1510558487737),
+            progress: {
+                front: 65,
+                back: 40,
+                pm: 50,
+                total: 48
+            },
+            leader: 'fuckman',
+        },
+        {
+            pic: '/images/bg13.png',
+            intro: 'Win8是Window通用平台开发平台的起点，Win8是Window通用平台开发平台的起点',
+            title: '基于人工智能的图片识别技术',
+            startTime: CUtil.ms2Date(1510558487737),
+            progress: {
+                front: 65,
+                back: 40,
+                pm: 50,
+                total: 48
+            },
+            leader: 'fuckman',
+        }
+    ]
+}
 
 
 /**
@@ -38,28 +150,26 @@ app.use(expressValidator());
 //     next()
 // })
 app.get('/', (req, res) => {
-    res.render('broadcast',{active:'broadcast'})
+    res.render('broadcast', {active: 'broadcast'})
 });
 app.get('/broadcast', (req, res) => {
-    res.render('broadcast',{active:'broadcast'})
+    res.render('broadcast', {active: 'broadcast'})
 });
 app.get('/home', (req, res) => {
-    res.render('home',{active:'home'})
+    res.render('home', tempData)
 });
 app.get('/projects', (req, res) => {
-    res.render('projects',{active:'projects'})
+    res.render('projects', {active: 'projects'})
 });
 app.get('/department', (req, res) => {
-    res.render('department',{active:'department'})
+    res.render('department', {active: 'department'})
 });
 app.get('/contact', (req, res) => {
-    res.render('contact',{active:'contact'})
+    res.render('contact', {active: 'contact'})
 });
 // app.get('/test/:id', (req, res) => {
 //     res.render('home')
 // });
-
-
 
 
 /**
---
+++ src/utils/Utils.ts
@@ -0,0 +1,47 @@
+class CUtils {
+    constructor() {
+
+    }
+
+    static ms2Date(msTime:number):object {
+        let _tempDate = new Date(msTime)
+        const monthFilter = (month) => {
+            switch (month) {
+                case '01':
+                    return "January";
+                case '02':
+                    return "February";
+                case '03':
+                    return "March";
+                case '04':
+                    return "April";
+                case '05':
+                    return "May";
+                case '06':
+                    return "June";
+                case '07':
+                    return "July";
+                case '08':
+                    return "August";
+                case '09':
+                    return "September";
+                case '10':
+                    return "October";
+                case '11':
+                    return "November";
+                case '12':
+                    return "December";
+            }
+        }
+        return {
+            year: _tempDate.getFullYear().toString(),
+            month: monthFilter((_tempDate.getMonth() + 1) > 9 ? (_tempDate.getMonth() + 1).toString() : '0' + (_tempDate.getMonth() + 1).toString()),
+            date: _tempDate.getDate() > 9 ? _tempDate.getDate().toString() : '0' + _tempDate.getDate().toString(),
+            hour: _tempDate.getHours() > 9 ? _tempDate.getHours().toString() : '0' + _tempDate.getHours().toString(),
+            minute: _tempDate.getMinutes() > 9 ? _tempDate.getMinutes().toString() : '0' + _tempDate.getMinutes().toString()
+        }
+    }
+
+}
+
+export default CUtils
